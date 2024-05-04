cf32bdf73f08dfccf1e47a2ee302bb5c7a70d21e
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-05-04 17:50:00 +0200

    update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, modifyProfile, getUsers } = require('../controllers/user.controller');
+const { register, login, modifyProfile } = require('../controllers/user.controller');
 const { isAuth } = require('../middleware/auth');
 
 
 router.post('/register', register);
 router.post('/login', login);
 router.put('/update', [isAuth], modifyProfile);
-router.get('/alluser', [isAuth], getUsers);
 
 module.exports = router;
