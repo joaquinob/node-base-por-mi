5bf18bcb33675c0ab86f9be00d9113eb9854e4b2
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-05-05 21:14:58 +0200

    update final

--- index.js
+++ index.js
@@ -5,20 +5,14 @@ const userRoutes = require('./src/routes/user.route');
 const playersRoutes = require('./src/routes/player.route');
 const cors = require('cors');
 
-
 const server = express();
 const PORT = process.env.PORT || 5002;
-
-/* Route prueba
-server.get('/', (req, res)=> {
-    res.send('Welcome to my api')
-})*/
 
 server.use(express.json());
 connectDB();
 server.use(cors());
 server.use('/user', userRoutes);
-server.use('/player', playersRoutes)
+server.use('/player', playersRoutes);
 
 server.listen(PORT, () => {
   console.log(`Escuchando puerto http://localhost:${PORT}`);
